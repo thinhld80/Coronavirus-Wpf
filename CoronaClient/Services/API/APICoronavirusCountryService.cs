de9b2c4be9d969d905cee54760b808cd5f84bba7
Thinh Le <thinhld80@gmail.com>
2020-10-18 11:45:23 +0700

    Add domain layer

---
+++ CoronaClient/Services/API/APICoronavirusCountryService.cs
@@ -0,0 +1,40 @@
+﻿using CoronaClient.Models;
+using CoronaClient.Services.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+
+namespace CoronaClient.Services.API
+{
+    public class APICoronavirusCountryService : ICoronavirusCountryServices
+    {
+        public async Task<IEnumerable<CoronavirusCountry>> GetTopCases(int amoutOfCountries)
+        {
+            using(HttpClient client = new HttpClient())
+            {
+                string requestUri = "https://corona.lmao.ninja/v3/covid-19/countries?sort=cases";
+                
+                HttpResponseMessage apiResponse = await client.GetAsync(requestUri);
+
+                string jsonResponse = await apiResponse.Content.ReadAsStringAsync();
+
+                List<APICoronavirusCountry> apiCountries = JsonSerializer.Deserialize<List<APICoronavirusCountry>>(jsonResponse, new JsonSerializerOptions()
+                {
+                    PropertyNameCaseInsensitive = true
+                }); ;
+
+                return apiCountries.Select(apiCountry => new CoronavirusCountry()
+                {
+                    CountryName = apiCountry.Country,
+                    CaseCount = apiCountry.Cases,
+                    FlagUri = apiCountry.CountryInfo.Flag
+                });
+            }
+        }
+    }
+}
---
+++ CoronaClient/Services/ICoronavirusCountryServices.cs
@@ -0,0 +1,13 @@
+﻿using CoronaClient.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoronaClient.Services
+{
+    public interface ICoronavirusCountryServices
+    {
+        Task<IEnumerable<CoronavirusCountry>> GetTopCases(int amoutOfCountries);
+    }
+}
