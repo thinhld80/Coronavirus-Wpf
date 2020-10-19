de15f5e4c4d850794524d9e27e2b3d3d896a8b09
thinhld80 <thinhld80@gmail.com>
2020-10-19 10:01:55 +0700

    Fix country name labels

--- CoronaClient/ViewModels/CoronavirusCountriesChartViewModel.cs
+++ CoronaClient/ViewModels/CoronavirusCountriesChartViewModel.cs
@@ -3,24 +3,53 @@ using CoronaClient.Services;
 using LiveCharts;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CoronaClient.ViewModels
 {
-    public class CoronavirusCountriesChartViewModel
+    public class CoronavirusCountriesChartViewModel : ViewModelBase
     {
         private const int AMOUNT_OF_COUNTRY = 10;
         private readonly ICoronavirusCountryService _coronavirusCountryServices;
 
-        public ChartValues<int> CoronavirusCountryCaseCounts { get; set; }
+        private ChartValues<int> _coronavirusCountryCaseCounts;
+        private ObservableCollection<string> _coronavirusCountryNames;
 
-        public string[] CoronavirusCountryNames { get; set; }
+
+        public ChartValues<int> CoronavirusCountryCaseCounts
+        {
+            get 
+            { 
+                return _coronavirusCountryCaseCounts; 
+            }
+            set 
+            { 
+                _coronavirusCountryCaseCounts = value;
+                OnPropertyChanged(nameof(CoronavirusCountryCaseCounts));
+            }
+        }
+
+
+        public ObservableCollection<string> CoronavirusCountryNames 
+        {
+            get
+            {
+                return _coronavirusCountryNames;
+            }
+            set
+            {
+                _coronavirusCountryNames = value;
+                OnPropertyChanged(nameof(CoronavirusCountryNames));
+            }
+        }
 
         public CoronavirusCountriesChartViewModel(ICoronavirusCountryService coronavirusCountryService)
         {
             _coronavirusCountryServices = coronavirusCountryService;
+
         }
 
         public static CoronavirusCountriesChartViewModel LoadViewModel(ICoronavirusCountryService coronavirusCountryService, Action<Task> onLoaded = null)
@@ -37,7 +66,7 @@ namespace CoronaClient.ViewModels
             IEnumerable<CoronavirusCountry> countries = await _coronavirusCountryServices.GetTopCases(AMOUNT_OF_COUNTRY);
 
             CoronavirusCountryCaseCounts = new ChartValues<int>(countries.Select(c => c.CaseCount));
-            CoronavirusCountryNames = countries.Select(c => c.CountryName).ToArray();
+            CoronavirusCountryNames = new ObservableCollection<string>(countries.Select(c => c.CountryName));
         }
     }
 }
